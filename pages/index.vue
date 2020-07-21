dc027678b3328f94c28f10e15080c2def1e15b3f
Rezuan Kassim <rezuankassim@hotmail.com>
2020-07-21 22:24:23 +0800

    seperate navbar component in default layout vue and fix reactive buttons and dropdown

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div></div>
-  <!-- <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+  <div class="bg-white shadow overflow-hidden sm:rounded-lg">
     <div class="px-4 py-5 border-b border-gray-200 sm:px-6">
       <h3 class="text-lg leading-6 font-medium text-gray-900">
         Applicant Information
@@ -10,7 +9,7 @@
         Personal details and application.
       </p>
     </div>
-  </div> -->
+  </div>
 </template>
 
 <script>
